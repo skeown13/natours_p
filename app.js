b4a73ee5f623564a9f6c9bb1c718e7fe785e65d4
skeown13 <code@keown.net>
2020-08-04 10:39:57 -0700

    created post request in order to add a new tour to the api

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 const fs = require("fs")
 
 const express = require("express")
+const { RSA_NO_PADDING } = require("constants")
 
 const app = express()
+
+// Middleware (function that can modify the incoming request data)
+app.use(express.json())
 
 // app.get("/", (req, res) => {
 //   res
@@ -28,6 +32,27 @@ app.get("/api/v1/tours", (req, res) => {
       tours: tours,
     },
   })
+})
+
+app.post("/api/v1/tours", (req, res) => {
+  // console.log(req.body)
+
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({ id: newId }, req.body)
+
+  tours.push(newTour)
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      })
+    }
+  )
 })
 
 const PORT = 3000
