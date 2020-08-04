e7279634f5d3684f95c26f95cd285611c90fcd62
skeown13 <code@keown.net>
2020-08-04 10:21:13 -0700

    created get request to the api that we have created (found in tours-simple.json)

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 // all express configurations in app.js
+const fs = require("fs")
 
 const express = require("express")
 
 const app = express()
 
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Hello from the server side!", app: "Natours" })
-})
+// app.get("/", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: "Hello from the server side!", app: "Natours" })
+// })
 
-app.post("/", (req, res) => {
-  res.send("You can post to this endpoint...")
+// app.post("/", (req, res) => {
+//   res.send("You can post to this endpoint...")
+// })
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+)
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      // can state just tours with es6 since the key is the same as the value.
+      tours: tours,
+    },
+  })
 })
 
 const PORT = 3000
