01123c50f54473fed543e975a6ea240b381c0728
skeown13 <code@keown.net>
2020-08-05 15:22:44 -0700

    included patch route for a tours at a specific id

--- app.js
+++ app.js
@@ -78,6 +78,22 @@ app.post("/api/v1/tours", (req, res) => {
   )
 })
 
+app.patch("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    })
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "<Updated (placeholder) tour here...>",
+    },
+  })
+})
+
 const PORT = 3000
 app.listen(PORT, () => {
   console.log(`App running on localhost:${PORT}`)
