6f9bba2e641938b75b0fec461a958810d37515f4
skeown13 <code@keown.net>
2020-08-07 09:22:17 -0700

    added route handlers/controllers for the Users routes. not fleshed out

--- app.js
+++ app.js
@@ -22,7 +22,12 @@ app.use((req, res, next) => {
 })
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+  fs.readFileSync(
+    `${__dirname}/dev-data/data/tours-simple.json`
+  )
+)
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/users.json`)
 )
 
 // ROUTE HANDLERS
@@ -114,20 +119,126 @@ const deleteTour = (req, res) => {
   })
 }
 
-// app.get("/api/v1/tours", getAllTours)
-// app.post("/api/v1/tours", createTour)
-// app.get("/api/v1/tours/:id", getTour)
-// app.patch("/api/v1/tours/:id", updateTour)
-// app.delete("/api/v1/tours/:id", deleteTour)
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!",
+  })
+  // console.log(req.requestTime)
+  // res.status(200).json({
+  //   status: "success",
+  //   requestedAt: req.requestTime,
+  //   results: users.length,
+  //   data: {
+  //     users,
+  //   },
+  // })
+}
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!",
+  })
+  // console.log(req.params)
+
+  // // * 1 against a string auto converts string to number
+  // const id = req.params.id * 1
+  // const user = users.find((el) => el.id === id)
+
+  // if (!user) {
+  //   return res.status(404).json({
+  //     status: "fail",
+  //     message: "Invalid ID",
+  //   })
+  // }
+
+  // res.status(200).json({
+  //   status: "success",
+  //   data: {
+  //     user,
+  //   },
+  // })
+}
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!",
+  })
+  // const newId = users[users.length - 1].id + 1
+  // const newUser = Object.assign({ id: newId }, req.body)
+
+  // users.push(newUser)
+  // fs.writeFile(
+  //   `${__dirname}/dev-data/data/users.json`,
+  //   JSON.stringify(users),
+  //   (err) => {
+  //     res.status(201).json({
+  //       status: "success",
+  //       data: {
+  //         user: newUser,
+  //       },
+  //     })
+  //   }
+  // )
+}
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!",
+  })
+  // if (req.params.id * 1 > users.length) {
+  //   return res.status(404).json({
+  //     status: "fail",
+  //     message: "Invalid ID",
+  //   })
+  // }
+
+  // res.status(200).json({
+  //   status: "success",
+  //   data: {
+  //     user: "<Updated (placeholder) user here...>",
+  //   },
+  // })
+}
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!",
+  })
+  // if (req.params.id * 1 > users.length) {
+  //   return res.status(404).json({
+  //     status: "fail",
+  //     message: "Invalid ID",
+  //   })
+  // }
+
+  // // status code 204 is "no content"
+  // res.status(204).json({
+  //   status: "success",
+  //   data: null,
+  // })
+}
 
 // ROUTES
 app.route("/api/v1/tours").get(getAllTours).post(createTour)
 
-app.route("/api/v1/tours/:id").get(getTour).patch(updateTour).delete(deleteTour)
+app
+  .route("/api/v1/tours/:id")
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour)
 
 app.route("/api/v1/users").get(getAllUsers).post(createUser)
 
-app.route("/api/v1/users/:id").get(getUser).patch(updateUser).delete(deleteUser)
+app
+  .route("/api/v1/users/:id")
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser)
 
 // START SERVER
 const PORT = 3000
