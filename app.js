42f9dc4cb09c2b515ef6734183e7b8aa02b43407
skeown13 <code@keown.net>
2020-08-11 18:07:46 -0700

    moved the tours route handlers and routes from app.js to tourRoutes.js

--- app.js
+++ app.js
@@ -31,93 +31,6 @@ const users = JSON.parse(
 )
 
 // ROUTE HANDLERS
-const getAllTours = (req, res) => {
-  console.log(req.requestTime)
-  res.status(200).json({
-    status: "success",
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      // can state just tours with es6 since the key is the same as the value.
-      tours: tours,
-    },
-  })
-}
-
-const getTour = (req, res) => {
-  console.log(req.params)
-
-  // * 1 against a string auto converts string to number
-  const id = req.params.id * 1
-  const tour = tours.find((el) => el.id === id)
-
-  // if (id > tours.length) {
-  if (!tour) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    })
-  }
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour,
-    },
-  })
-}
-
-const createTour = (req, res) => {
-  // console.log(req.body)
-
-  const newId = tours[tours.length - 1].id + 1
-  const newTour = Object.assign({ id: newId }, req.body)
-
-  tours.push(newTour)
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        data: {
-          tour: newTour,
-        },
-      })
-    }
-  )
-}
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    })
-  }
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "<Updated (placeholder) tour here...>",
-    },
-  })
-}
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    })
-  }
-
-  // status code 204 is "no content"
-  res.status(204).json({
-    status: "success",
-    data: null,
-  })
-}
 
 const getAllUsers = (req, res) => {
   res.status(500).json({
@@ -224,16 +137,7 @@ const deleteUser = (req, res) => {
 }
 
 // ROUTES
-const tourRouter = express.Router()
 const userRouter = express.Router()
-
-tourRouter.route("/").get(getAllTours).post(createTour)
-
-tourRouter
-  .route("/:id")
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour)
 
 userRouter.route("/").get(getAllUsers).post(createUser)
 
