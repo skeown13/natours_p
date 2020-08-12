61404189749549e7f8486201ed03c0592c6f7452
skeown13 <code@keown.net>
2020-08-12 15:16:05 -0700

    moved the tour functions from routes/ to controllers/ and required it back in to routes/

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,108 +1,17 @@
-const fs = require("fs")
 const express = require("express")
-
-const tours = JSON.parse(
-  fs.readFileSync(
-    `${__dirname}/../dev-data/data/tours-simple.json`
-  )
-)
-
-const getAllTours = (req, res) => {
-  console.log(req.requestTime)
-  res.status(200).json({
-    status: "success",
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      // can state just tours with es6 since the key is the same as the value.
-      tours: tours,
-    },
-  })
-}
-
-const getTour = (req, res) => {
-  console.log(req.params)
-
-  // * 1 against a string auto converts string to number
-  const id = req.params.id * 1
-  const tour = tours.find((el) => el.id === id)
-
-  // if (id > tours.length) {
-  if (!tour) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    })
-  }
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour,
-    },
-  })
-}
-
-const createTour = (req, res) => {
-  // console.log(req.body)
-
-  const newId = tours[tours.length - 1].id + 1
-  const newTour = Object.assign({ id: newId }, req.body)
-
-  tours.push(newTour)
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        data: {
-          tour: newTour,
-        },
-      })
-    }
-  )
-}
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    })
-  }
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "<Updated (placeholder) tour here...>",
-    },
-  })
-}
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    })
-  }
-
-  // status code 204 is "no content"
-  res.status(204).json({
-    status: "success",
-    data: null,
-  })
-}
+const tourController = require("./../controllers/tourController")
 
 const router = express.Router()
 
-router.route("/").get(getAllTours).post(createTour)
+router
+  .route("/")
+  .get(tourController.getAllTours)
+  .post(tourController.createTour)
 
 router
   .route("/:id")
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour)
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour)
 
 module.exports = router
