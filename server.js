9ec8036fb39b525889a77a2c1e38a7351d1a4a55
skeown13 <code@keown.net>
2020-08-13 11:54:06 -0700

    informative comment added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Starting file where everything starts
 const dotenv = require("dotenv")
 // must run the config file before requiring the app file so that the app file has access to the config info
+// only have to do this once and oce it is done all of the config info is added to the process.env which is inately accessible by the entire application
 dotenv.config({ path: "./config.env" })
 
 const app = require("./app")
