664b8b1706875920a20c3f7cfcd8d181c3f57c1d
skeown13 <code@keown.net>
2020-08-13 11:52:59 -0700

    required dotenv and set up the config file to be accessed by the entire application. updated how the port is called

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 // Starting file where everything starts
+const dotenv = require("dotenv")
+// must run the config file before requiring the app file so that the app file has access to the config info
+dotenv.config({ path: "./config.env" })
+
 const app = require("./app")
 
-const PORT = 3000
-app.listen(PORT, () => {
-  console.log(`App running on localhost:${PORT}`)
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+  console.log(`App running on localhost:${port}`)
 })
