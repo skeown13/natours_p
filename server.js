a85047631bb61a0f8f70a7023436ecfeb22282b1
skeown13 <code@keown.net>
2020-08-18 14:36:55 -0700

    required mongoose and connected to our database

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 // Starting file where everything starts
+const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 // must run the config file before requiring the app file so that the app file has access to the config info
 // only have to do this once and oce it is done all of the config info is added to the process.env which is inately accessible by the entire application
 dotenv.config({ path: "./config.env" })
 
 const app = require("./app")
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+)
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB connection successfull!"))
 
 const port = process.env.PORT || 8000
 app.listen(port, () => {
