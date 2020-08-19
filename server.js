c24378bf04d63f37bde26544a138524d46faa0a4
skeown13 <code@keown.net>
2020-08-19 15:01:41 -0700

    created a basic tourSchema with Mongoose

--- server.js
+++ server.js
@@ -20,6 +20,22 @@ mongoose
   })
   .then(() => console.log("DB connection successfull!"))
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name!"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price!"],
+  },
+})
+
 const port = process.env.PORT || 8000
 app.listen(port, () => {
   console.log(`App running on localhost:${port}`)
