44339b9a7cc2666b4e356a9acd468d118924a44f
skeown13 <code@keown.net>
2020-08-19 15:49:24 -0700

    moved the tourSchema/Model from server.js to tourModel.js

--- server.js
+++ server.js
@@ -21,37 +21,6 @@ mongoose
   })
   .then(() => console.log("DB connection successfull!"))
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "A tour must have a name!"],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, "A tour must have a price!"],
-  },
-})
-
-const Tour = mongoose.model("Tour", tourSchema)
-
-// New Document created from the Tour model
-const testTour = new Tour({
-  name: "The Park Camper",
-  price: 997,
-})
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc)
-  })
-  .catch((err) => console.log("ERROR!!", err))
-
 const port = process.env.PORT || 8000
 app.listen(port, () => {
   console.log(`App running on localhost:${port}`)
