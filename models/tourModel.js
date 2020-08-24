1e9fb5de2c84c347f7967f4063bcff3847210368
skeown13 <code@keown.net>
2020-08-24 15:58:47 -0700

    using validators from an external library

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const slugify = require("slugify")
+const validator = require("validator")
 
 const tourSchema = new mongoose.Schema(
   {
@@ -10,6 +11,10 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, "A tour name must have less or equal than 40 characters"],
       minlength: [10, "A tour name must have more or equal than 10 characters"],
+      // validate: [
+      //   validator.isAlpha,
+      //   "Tour name must only contain alphabetical characters",
+      // ], // We are not calling it here. Just specifying a function that is to be used.
     },
     slug: String,
     duration: {
