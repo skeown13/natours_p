55fced8a94bb65e6b33673ebfa82a63866af3286
skeown13 <code@keown.net>
2020-08-25 10:40:58 -0700

    handling any unhandled routes at the end of app.js using app.all()

--- app.js
+++ app.js
@@ -12,14 +12,10 @@ const app = express()
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"))
 }
+
 app.use(express.json())
 
 app.use(express.static(`${__dirname}/public`))
-
-app.use((req, res, next) => {
-  console.log("Hello from the Middleware!!!")
-  next()
-})
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
@@ -29,5 +25,13 @@ app.use((req, res, next) => {
 // ROUTES
 app.use("/api/v1/tours", tourRouter)
 app.use("/api/v1/users", userRouter)
+
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  })
+  next()
+})
 
 module.exports = app
