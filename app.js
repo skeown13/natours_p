0f3909e4b5e5a73fee8ac9799ed1f0f20619c46f
skeown13 <code@keown.net>
2020-08-25 13:54:39 -0700

    updated the app.all middleware to use a new instance of the AppError class

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express")
 const morgan = require("morgan")
 
+const AppError = require("./utils/appError")
+
 const tourRouter = require("./routes/tourRoutes")
 const userRouter = require("./routes/userRoutes")
 
@@ -27,19 +29,12 @@ app.use("/api/v1/tours", tourRouter)
 app.use("/api/v1/users", userRouter)
 
 app.all("*", (req, res, next) => {
-  // res.status(404).json({
-  //   status: "fail",
-  //   message: `Can't find ${req.originalUrl} on this server!`,
-  // })
-  // next()
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`)
-  err.status = "fail"
-  err.statusCode = 404
-
-  next(err)
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
 })
 
 app.use((err, req, res, next) => {
+  console.log(err.stack)
+
   err.statusCode = err.statusCode || 500
   err.status = err.status || "error"
 
