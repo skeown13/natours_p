fc3ba98233f7899ee024e3c990fc78b47461d591
skeown13 <code@keown.net>
2020-08-25 11:09:22 -0700

    created a centralized Error Handling Middleware

--- app.js
+++ app.js
@@ -34,4 +34,15 @@ app.all("*", (req, res, next) => {
   next()
 })
 
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || "error"
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  })
+  next()
+})
+
 module.exports = app
