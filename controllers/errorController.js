8b1a093c54370c28d6bd4788c37c6fe0ac67b425
skeown13 <code@keown.net>
2020-08-26 14:40:39 -0700

    when in production distinguish between operational errors that we trust and errors that are programming/unknown

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -8,10 +8,24 @@ const sendErrorDev = (err, res) => {
 }
 
 const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  })
+  //Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    })
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error("ERROR!", err)
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong!",
+    })
+  }
 }
 
 module.exports = (err, req, res, next) => {
