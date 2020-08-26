c765e6250a577babfd3c1d693df940442efaaa3a
skeown13 <code@keown.net>
2020-08-26 14:19:39 -0700

    added 404 errors if an incorrect id is included. using next(new AppError())

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require("../models/tourModel")
 const APIFeatures = require("../utils/apiFeatures")
 const catchAsync = require("../utils/catchAsync")
+const AppError = require("../utils/appError")
 
 exports.getTopTours = (req, res, next) => {
   // limit=5&sort=-ratingsAverage,price
@@ -34,6 +35,10 @@ exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id)
   // Tour.findOne({ _id: req.params.id })
 
+  if (!tour) {
+    return next(new AppError("No tour found with that ID", 404))
+  }
+
   res.status(200).json({
     status: "success",
     data: {
@@ -61,6 +66,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     runValidators: true,
   })
 
+  if (!tour) {
+    return next(new AppError("No tour found with that ID", 404))
+  }
+
   res.status(200).json({
     status: "success",
     data: {
@@ -70,7 +79,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 })
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id)
+  const tour = await Tour.findByIdAndDelete(req.params.id)
+
+  if (!tour) {
+    return next(new AppError("No tour found with that ID", 404))
+  }
 
   // status code 204 is "no content"
   res.status(204).json({
