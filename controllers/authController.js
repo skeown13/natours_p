38a18c9c019158d069c168a73181c2d26620acf1
skeown13 <code@keown.net>
2020-09-01 17:33:23 -0700

    created a login function that checks if email and password are given, then whether they exist/match in the database, and finally returns a token (currently a placeholder token)

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken")
 
 const User = require("../models/userModel")
 const catchAsync = require("../utils/catchAsync")
+const AppError = require("../utils/appError")
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -24,12 +25,26 @@ exports.signup = catchAsync(async (req, res, next) => {
   })
 })
 
-exports.login = (req, res, next) => {
+exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body
 
-  // 1) Check in email and passwords exist
+  // 1) Check if email and passwords exist
+  if (!email || !password) {
+    return next(new AppError("Please provide email and password!", 400))
+  }
 
   // 2) Check if the user exists && password is correct
+  // user is a document of the User collection therefore we can use the instance method on it.
+  const user = await User.findOne({ email }).select("+password")
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError("Incorrect Email or Password", 401))
+  }
 
   // 3) If everything is okay, send the token to client
-}
+  const token = ""
+  res.status(200).json({
+    status: "success",
+    token,
+  })
+})
