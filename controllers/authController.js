2e8082dcf1b5d9abd0b7f435583ebe4dab2fd7b3
skeown13 <code@keown.net>
2020-09-10 19:22:27 -0700

    removed the function that creates a token that is needed for login/verification to utils/sendToken.js - require it in at the top as createSendToken

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,24 +6,7 @@ const User = require("../models/userModel")
 const catchAsync = require("../utils/catchAsync")
 const AppError = require("../utils/appError")
 const sendEmail = require("../utils/email")
-
-const signToken = id => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  })
-}
-
-const createSendToken = (user, statusCode, res) => {
-  const token = signToken(user._id)
-
-  res.status(statusCode).json({
-    status: "success",
-    token,
-    data: {
-      user,
-    },
-  })
-}
+const createSendToken = require("../utils/sendToken")
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body)
