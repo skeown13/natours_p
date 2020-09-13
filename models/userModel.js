ef4e16ad45e48cbcd9e4c58183068c4121a3a064
skeown13 <code@keown.net>
2020-09-13 12:27:12 -0700

    added a property of 'active' to the user model that is Boolean, deafaulted to true, and made invisible

--- models/userModel.js
+++ models/userModel.js
@@ -41,6 +41,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 })
 
 // MIDDLEWARE
