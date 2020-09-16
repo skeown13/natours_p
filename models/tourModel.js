8523e401b20e370df65e8f9a10184cba3f1b2710
skeown13 <code@keown.net>
2020-09-16 15:51:42 -0700

    embedded 'guide' users into the tourModel

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 const slugify = require("slugify")
-const validator = require("validator")
+const User = require("./userModel")
+// const validator = require("validator")
 
 const tourSchema = new mongoose.Schema(
   {
@@ -108,6 +109,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -124,6 +126,13 @@ tourSchema.virtual("durationWeeks").get(function () {
 // DOCUMENT MIDDLEWARE: runs before .save() and .create() **not on .insertMany()**
 tourSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true })
+  next()
+})
+
+tourSchema.pre("save", async function (next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id))
+  // must use Promise.all because guidesPromises is an array of promises that we then run by awaiting Promise.all()
+  this.guides = await Promise.all(guidesPromises)
   next()
 })
 
