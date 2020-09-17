493136707128a433954f482d326346e0548e3797
skeown13 <code@keown.net>
2020-09-16 17:18:30 -0700

    creation of reviewModel

---
+++ models/reviewModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, "A review must have a review!"],
+    },
+    rating: {
+      type: Number,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Tour",
+      required: [true, "Review must belong to a tour!"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Review must belong to a user!"],
+    },
+  },
+  // virtuals: when we have a virtual property - a field that is not stored in the database, but calculated using some other value we want this to show up when there is an output
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+)
+
+const Review = mongoose.model("Review", reviewSchema)
+
+module.exports = Review
