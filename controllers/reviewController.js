c22276ed2d6f3efff39d8985fbd51b0bcf81ea87
skeown13 <code@keown.net>
2020-09-18 14:42:19 -0700

    changed status in createReview middleware from 200 to 201 (created)

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -18,7 +18,7 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 exports.createReview = catchAsync(async (req, res, next) => {
   const newReview = await Review.create(req.body)
 
-  res.status(200).json({
+  res.status(201).json({
     status: "success",
     data: {
       review: newReview,
