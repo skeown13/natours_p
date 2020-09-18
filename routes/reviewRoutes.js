0cbfc9b8c921fd7dc7b1fa3df9efaff23ccdc730
skeown13 <code@keown.net>
2020-09-17 17:41:56 -0700

    required express and reviewController. using express.Router(). created basic get and post routes to the '/' route

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,1 +1,11 @@
 // get all reviews and create new review
+
+const express = require("express")
+const reviewController = require("../controllers/reviewController")
+
+const router = express.Router()
+
+router
+  .route("/")
+  .get(reviewController.getAllReviews)
+  .post(reviewController.createReview)
