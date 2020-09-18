ce1d741962ca38eb8f8358d0ed2f54ca82230091
skeown13 <code@keown.net>
2020-09-18 14:32:38 -0700

    required reviewRoutes.js and app.use(d) it

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require("./controllers/errorController")
 
 const tourRouter = require("./routes/tourRoutes")
 const userRouter = require("./routes/userRoutes")
+const reviewRouter = require("./routes/reviewRoutes")
 
 const app = express()
 
@@ -71,6 +72,7 @@ app.use((req, res, next) => {
 // ROUTES
 app.use("/api/v1/tours", tourRouter)
 app.use("/api/v1/users", userRouter)
+app.use("/api/v1/reviews", reviewRouter)
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
