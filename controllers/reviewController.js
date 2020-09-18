c33b3ae75297ebe83364473fcce84392d21c06e7
skeown13 <code@keown.net>
2020-09-18 14:14:06 -0700

    created middleware function for getAllReviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,10 +1,24 @@
 const Review = require("../models/reviewModel")
+const APIFeatures = require("../utils/apiFeatures")
 const catchAsync = require("../utils/catchAsync")
 const AppError = require("../utils/appError")
 
-exports.getAllReviews = (req, res, next) => {
-  next()
-}
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Review.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate()
+  const reviews = await features.query
+
+  res.status(200).json({
+    status: "success",
+    results: reviews.length,
+    data: {
+      reviews,
+    },
+  })
+})
 
 exports.createReview = (req, res, next) => {
   next()
