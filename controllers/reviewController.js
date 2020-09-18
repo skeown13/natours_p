b739a9c54d7aab8a5e15000701cb9f068974a8fd
skeown13 <code@keown.net>
2020-09-17 18:28:42 -0700

    scaffolded out the middleware functions getAllReviews and createReview

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,3 +1,11 @@
 const Review = require("../models/reviewModel")
 const catchAsync = require("../utils/catchAsync")
 const AppError = require("../utils/appError")
+
+exports.getAllReviews = (req, res, next) => {
+  next()
+}
+
+exports.createReview = (req, res, next) => {
+  next()
+}
