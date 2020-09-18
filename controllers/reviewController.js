1b2c8ce14570a16fda695e1452e0563afe250cc4
skeown13 <code@keown.net>
2020-09-18 14:39:48 -0700

    removed the apiFeatures portion from the getAllReviews middleware

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -4,12 +4,7 @@ const catchAsync = require("../utils/catchAsync")
 const AppError = require("../utils/appError")
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const features = new APIFeatures(Review.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate()
-  const reviews = await features.query
+  const reviews = await Review.find()
 
   res.status(200).json({
     status: "success",
