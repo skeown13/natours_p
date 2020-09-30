e31959295f9f44f853d69876f731530d6d453197
skeown13 <code@keown.net>
2020-09-30 15:42:52 -0700

    created a getOne middleware in the handlerFactory that way it may be used in all controllers. had to save the query to a variable before awaiting it so that it may be manipulated

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -50,3 +50,22 @@ exports.createOne = Model =>
       },
     })
   })
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    // Instead of awaiting the query right away we are first saving the query to a variable so that we can manipulate the variable if necessary and then awaiting it
+    let query = Model.findById(req.params.id)
+    if (popOptions) query = query.populate(popOptions)
+    const doc = await query
+
+    if (!doc) {
+      return next(new AppError("No document found with that ID", 404))
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: doc,
+      },
+    })
+  })
