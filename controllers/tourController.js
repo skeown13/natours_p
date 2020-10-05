6ad093ef67386661c0c3faf12edd778d0070992b
skeown13 <code@keown.net>
2020-10-05 15:33:24 -0700

    code/comment clean up

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,7 @@
 const Tour = require("../models/tourModel")
 const catchAsync = require("../utils/catchAsync")
-const AppError = require("../utils/appError")
 const factory = require("./handlerFactory")
+// const AppError = require("../utils/appError")
 
 exports.getTopTours = (req, res, next) => {
   // limit=5&sort=-ratingsAverage,price
@@ -16,20 +16,6 @@ exports.getTour = factory.getOne(Tour, { path: "reviews" })
 exports.createTour = factory.createOne(Tour)
 exports.updateTour = factory.updateOne(Tour)
 exports.deleteTour = factory.deleteOne(Tour)
-
-// exports.deleteTour = catchAsync(async (req, res, next) => {
-//   const tour = await Tour.findByIdAndDelete(req.params.id)
-
-//   if (!tour) {
-//     return next(new AppError("No tour found with that ID", 404))
-//   }
-
-//   // status code 204 is "no content"
-//   res.status(204).json({
-//     status: "success",
-//     data: null,
-//   })
-// })
 
 exports.getTourStats = catchAsync(async (req, res, next) => {
   const stats = await Tour.aggregate([
