9aba4d8031e38f5604d0b6fc83d71e18c7b9fca6
skeown13 <code@keown.net>
2020-10-05 15:27:07 -0700

    hack in order to allow for nested GET reviews on tour

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -72,8 +72,12 @@ exports.getOne = (Model, popOptions) =>
 
 exports.getAll = Model =>
   catchAsync(async (req, res, next) => {
+    // To allow for nested GET reviews on tour(hack)
+    let filter = {}
+    if (req.params.tourId) filter = { tour: req.params.tourId }
+
     // EXECUTE QUERY
-    const features = new APIFeatures(Model.find(), req.query)
+    const features = new APIFeatures(Model.find(filter), req.query)
       .filter()
       .sort()
       .limitFields()
