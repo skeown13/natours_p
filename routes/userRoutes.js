7b59d1b752447c05bc4ed53bb4837e0a13f7cd35
skeown13 <code@keown.net>
2020-10-08 15:31:49 -0700

    did a router.use(authController.protect) so that all routes that come after it check that first since all it is is middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,22 +6,13 @@ const router = express.Router()
 
 router.post("/signup", authController.signup)
 router.post("/login", authController.login)
-
 router.post("/forgotPassword", authController.forgotPassword)
 router.patch("/resetPassword/:token", authController.resetPassword)
-router.patch(
-  "/updatePassword",
-  authController.protect,
-  authController.updatePassword
-)
 
-router.get(
-  "/me",
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-)
+router.use(authController.protect)
 
+router.patch("/updatePassword", authController.updatePassword)
+router.get("/me", userController.getMe, userController.getUser)
 router.patch("/updateMe", authController.protect, userController.updateMe)
 // We are not actually deleting the user from the database, but since the user will no longer be accessible from anywhere it is still okay to use the http method of delete
 router.delete("/deleteMe", authController.protect, userController.deleteMe)
