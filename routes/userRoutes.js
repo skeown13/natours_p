9523a019b03815191c0148a37d63e908a6f471a9
skeown13 <code@keown.net>
2020-10-08 15:34:26 -0700

    did a router.use(authController.restrictTo('admin')) so that all routes that come after it check that first since all it is is middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,7 @@ router.post("/login", authController.login)
 router.post("/forgotPassword", authController.forgotPassword)
 router.patch("/resetPassword/:token", authController.resetPassword)
 
+// Protect all routes after this middleware
 router.use(authController.protect)
 
 router.patch("/updatePassword", authController.updatePassword)
@@ -16,6 +17,9 @@ router.get("/me", userController.getMe, userController.getUser)
 router.patch("/updateMe", authController.protect, userController.updateMe)
 // We are not actually deleting the user from the database, but since the user will no longer be accessible from anywhere it is still okay to use the http method of delete
 router.delete("/deleteMe", authController.protect, userController.deleteMe)
+
+// Only admins can access the routes below this
+router.use(authController.restrictTo("admin"))
 
 router
   .route("/")
