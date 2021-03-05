5a75b0a58e139c51ba55b3f5797436d9f600c215
skeown13 <code@keown.net>
2021-03-05 13:39:36 -0800

    protecting the review routes. on specific reviews only user and admin can update/delete reviews

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -7,20 +7,30 @@ const authController = require("../controllers/authController")
 // Option of mergeParams: true allows for params such as the :tourId param in tourRoutes.js to be merged (accessed) here in the reviewRoutes.js
 const router = express.Router({ mergeParams: true })
 
+router.use(authController.protect)
+
 router
   .route("/")
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo("user"),
     reviewController.setTourUserIds,
     reviewController.createReview
   )
 
+// admin can edit/delete reviews
+// guides cannot add/edit/delete reviews
+
 router
   .route("/:id")
   .get(reviewController.getReview)
-  .patch(authController.protect, reviewController.updateReview)
-  .delete(authController.protect, reviewController.deleteReview)
+  .patch(
+    authController.restrictTo("user", "admin"),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo("user", "admin"),
+    reviewController.deleteReview
+  )
 
 module.exports = router
